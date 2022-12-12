23d4d1f5add2de7c8153a379348b14a79d413c4e
fyviezhao <zhaofw939@gmail.com>
2022-12-12 20:54:41 +0800

    initial upload

---
+++ apply_net_show.sh
@@ -0,0 +1,5 @@
+python apply_net.py show \
+configs/densepose_rcnn_R_101_FPN_DL_s1x.yaml \
+https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_101_FPN_DL_s1x/165712116/model_final_844d15.pkl \
+/path/to/img_or_imgs_dir \
+dp_v -v
